eb214d8e97c5e79c7b5ddf82cd06ea7d99fe1bbd
Javier-201906795 <88503776+Javier-201906795@users.noreply.github.com>
2023-02-18 11:44:45 -0600

    practica1

---
+++ Practica1/main.py
@@ -0,0 +1,21 @@
+#╔═════════════════╗
+#║   Practica 1    ║
+#║ Javier Yllescas ║
+#║    201906795    ║
+#╚═════════════════╝
+
+from mensajes import *
+
+#Pantalla Inicial
+print(pantallapricipal())
+input()
+
+#Menu Principal
+print(menuprincipal())
+
+
+#Observar seleccion
+opcion = 0
+while opcion != 5:
+    opcion = int(input())
+    
---
+++ Practica1/mensajes.py
@@ -0,0 +1,35 @@
+
+
+def pantallapricipal():
+    m =  """╔════════════════════════════════════╗\n"""
+    m += """║Lenguajes Formales y de Programacion║\n"""
+    m += """║              Seccion B-            ║\n"""
+    m += """║------------------------------------║\n"""
+    m += """║Carne: 201906795                    ║\n"""
+    m += """║Javier Ricardo Yllescas Barrios     ║\n"""
+    m += """╚════════════════════════════════════╝"""
+    m += """\nPresione una tecla para continuar...\n"""
+
+    
+    return m
+
+
+def menuprincipal():
+    m =  """█▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀█\n"""
+    m += """█            MENU               █\n"""
+    m += """█▀▀▀▀▀▀█▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀█\n"""
+    m += """█Opcion█    Descripcion         █\n"""
+    m += """█▀▀▀▀▀▀█▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀█\n"""
+    m += """█  1   █ Cargar archivo entrada █\n"""
+    m += """█▀▀▀▀▀▀█▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀█\n"""
+    m += """█  2   █ Gestionar peliculas    █\n"""
+    m += """█▀▀▀▀▀▀█▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀█\n"""
+    m += """█  3   █ Filtrado               █\n"""
+    m += """█▀▀▀▀▀▀█▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀█\n"""
+    m += """█  4   █ Grafica                █\n"""
+    m += """█▀▀▀▀▀▀█▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀█\n"""
+    m += """█  5   █ Salir                  █\n"""
+    m += """█▄▄▄▄▄▄█▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄█\n"""
+    m += """\n Seleccione una opcion y luego presioes ENTER...\n"""
+    
+    return m
