a06ce5276c68f5f7e0fdd314e16c61d5ef134ba2
Javier-201906795 <88503776+Javier-201906795@users.noreply.github.com>
2023-02-22 17:55:19 -0600

    mostrar actores
    muestra los actores de las peliculas

--- Practica1/gestionarpeliculas.py
+++ Practica1/gestionarpeliculas.py
@@ -20,12 +20,19 @@ def mostraractores(ListaPeliculas):
     print("\n \n")
 
     print("Seleccione una pelicula para ver sus actores. ")
-    
+    #Evalu si existe la opcion
+    opcion = None
     while True:
         opcion = input("Pelicula a Buscar: ")
-        if opcion == 5 or opcion == "5":
+        if opcion == "exit":
             break
         if validadoropcionespelicuals(opcion,len(ListaPeliculas)):
             break
+    #Imprime los actores
+    print("---------[ ACTORES ]---------")
+    for i in ListaPeliculas[int(opcion)-1].actores:
+        print(i)
+    # print(ListaPeliculas[int(opcion)].actores)
+    print("--------------------------------")
 
     return None
