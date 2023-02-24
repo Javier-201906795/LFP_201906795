c9411f32d268e88c1e9663ac6d773493b6147342
Javier-201906795 <88503776+Javier-201906795@users.noreply.github.com>
2023-02-23 23:29:25 -0600

    1

--- Practica1/filtrado.py
+++ Practica1/filtrado.py
@@ -11,15 +11,19 @@ def filtraadoporactor(ListaPeliculas):
         temp_lista_actores = i.actores
         #obtener actores
         for actor in temp_lista_actores:
-            
+            print(actor)
             if validadorrepetido(actor,temp_lista_actores):
                 #guardar
                 listaActores.append(actor)
                     
-            
+    
+    #Imprimir
     print("----")
+    cont = 0
     for j in listaActores:
-        print(j)
+        cont += 1
+        print(cont," | ", j)
+        
     
     print("----")
     input()
