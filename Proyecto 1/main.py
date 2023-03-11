6f386eb6a602282a3c1c5c855dbd6c6242d94a18
Javier-201906795 <88503776+Javier-201906795@users.noreply.github.com>
2023-03-11 13:45:19 -0600

    tkinter buttones
    probando el uso de botones

--- Proyecto 1/main.py
+++ Proyecto 1/main.py
@@ -1,4 +1,5 @@
 import tkinter
+import tkinter.messagebox
 
 
 
@@ -14,9 +15,31 @@ def saludo():
 def salir():
     ventana.destroy()
 
+
 boton = tkinter.Button(ventana,text="Hola", command = saludo, fg="red")
 boton.pack()
 boton.place(x=200, y=100)
+
+
+def test1():
+    tkinter.messagebox.showinfo("title", "MENSAJE NORMAL")
+
+boton = tkinter.Button(ventana,text="TEST", command = test1, fg="red")
+boton.pack()
+boton.place(x=200, y=200)
+
+
+def test2():
+    respueta = tkinter.messagebox.askquestion("title", "MENSAJE NORMAL")
+    if respueta == "yes":
+        tkinter.messagebox.showinfo("title", "MENSAJE NORMAL")
+    else:
+        tkinter.messagebox.showinfo("title", "No")
+
+boton = tkinter.Button(ventana,text="TEST2", command = test2, fg="red")
+boton.pack()
+boton.place(x=200, y=200)
+
 
 ventana.mainloop()
 
