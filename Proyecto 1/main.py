080bfca69a09f2ee3d55a78c864d0e6f11b687a8
Javier-201906795 <88503776+Javier-201906795@users.noreply.github.com>
2023-03-11 11:22:36 -0600

    Create main.py

---
+++ Proyecto 1/main.py
@@ -0,0 +1,16 @@
+import tkinter
+
+
+
+ventana = tkinter.Tk()
+ventana.title("[LFP]Proyecto1_201906795")
+ventana.geometry("500x500")
+
+cabezera = tkinter.Label(ventana,text = "Bienvenidos").pack()
+
+
+
+ventana.mainloop()
+
+
+
