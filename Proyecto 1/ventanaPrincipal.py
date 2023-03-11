fb4d9f517591b83b3a031d5b0cb2416c85bd9cee
Javier-201906795 <88503776+Javier-201906795@users.noreply.github.com>
2023-03-11 15:01:05 -0600

    tkinter
    probando funcinoes

--- Proyecto 1/ventanaPrincipal.py
+++ Proyecto 1/ventanaPrincipal.py
@@ -4,5 +4,8 @@ def ventanprincipal(ventana):
     ventana.title("[LFP]Proyecto1_201906795")
     ventana.geometry("500x500")
 
-    cabezera = tkinter.Label(ventana, text = "Bienvenidos").pack()
+    cabezera = tkinter.Label(ventana, text = "Archivo")
+    cabezera.config(bg="#FFFF00", font=("Arial",24))
+    cabezera.place(relx=0.01, rely=0.05, anchor='sw')
 
+    # tkinter.Label(ventana, text="Position 1 : x=0, y=0", bg="#FFFF00", fg="white").place(x=50, y=50)
