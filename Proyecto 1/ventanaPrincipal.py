5b1ddba9895cac0b053c462f2ade513aab5d7e85
Javier-201906795 <88503776+Javier-201906795@users.noreply.github.com>
2023-03-11 14:14:24 -0600

    estructura ventana principal

---
+++ Proyecto 1/ventanaPrincipal.py
@@ -0,0 +1,8 @@
+import tkinter 
+
+def ventanprincipal(ventana):
+    ventana.title("[LFP]Proyecto1_201906795")
+    ventana.geometry("500x500")
+
+    cabezera = tkinter.Label(ventana, text = "Bienvenidos").pack()
+
---
+++ Proyecto 1/ventanaTest.py
@@ -0,0 +1,45 @@
+import tkinter
+import tkinter.messagebox
+
+
+
+ventana = tkinter.Tk()
+ventana.title("[LFP]Proyecto1_201906795")
+ventana.geometry("500x500")
+
+cabezera = tkinter.Label(ventana, text = "Bienvenidos").pack()
+
+def saludo():
+    tkinter.Label(ventana, text = "Hola mundo").pack()
+
+def salir():
+    ventana.destroy()
+
+
+boton = tkinter.Button(ventana,text="Hola", command = saludo, fg="red")
+boton.pack()
+boton.place(x=200, y=100)
+
+
+def test1():
+    tkinter.messagebox.showinfo("title", "MENSAJE NORMAL")
+
+boton = tkinter.Button(ventana,text="TEST", command = test1, fg="red")
+boton.pack()
+boton.place(x=200, y=200)
+
+
+def test2():
+    respueta = tkinter.messagebox.askquestion("title", "MENSAJE NORMAL")
+    if respueta == "yes":
+        tkinter.messagebox.showinfo("title", "MENSAJE NORMAL")
+    else:
+        tkinter.messagebox.showinfo("title", "No")
+
+boton = tkinter.Button(ventana,text="TEST2", command = test2, fg="red")
+boton.pack()
+boton.place(x=200, y=250)
+
+
+ventana.mainloop()
+
