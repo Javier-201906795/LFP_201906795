44c48a713522eb48acc6bf1d027d5f7b64fffb2b
Javier-201906795 <88503776+Javier-201906795@users.noreply.github.com>
2023-03-11 13:37:43 -0600

    tkinter probando

--- Proyecto 1/main.py
+++ Proyecto 1/main.py
@@ -6,9 +6,17 @@ ventana = tkinter.Tk()
 ventana.title("[LFP]Proyecto1_201906795")
 ventana.geometry("500x500")
 
-cabezera = tkinter.Label(ventana,text = "Bienvenidos").pack()
+cabezera = tkinter.Label(ventana, text = "Bienvenidos").pack()
 
+def saludo():
+    tkinter.Label(ventana, text = "Hola mundo").pack()
 
+def salir():
+    ventana.destroy()
+
+boton = tkinter.Button(ventana,text="Hola", command = saludo, fg="red")
+boton.pack()
+boton.place(x=200, y=100)
 
 ventana.mainloop()
 
